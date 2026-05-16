8b66c100654e63a8ec7b722c6c40a50f71500235
Ayan <sujon.seo.bd@gmail.com>
2026-05-16 19:45:39 +0600

    added neon db postgras

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,54 @@
 import express, { type Application, type Request, type Response } from "express";
 const app: Application = express()
 const port = 3000
+import {Pool} from "pg"
+
+
+
+app.use(express.json());
+
+
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_NraTC2pFQg6y@ep-broad-butterfly-aqg9pi7w-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+})
+
+
+
+
+const initDB = async() => {
+    try {
+        await pool.query(`CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            idActive BOOLEAN DEFAULT TRUE,
+            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`)
+        console.log("Database initialized successfully")
+    } catch (error) {
+        console.log("Error initializing database:", error)
+    }
+}
+
+initDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req : Request, res : Response) => {
   res.status(200).json({ 
